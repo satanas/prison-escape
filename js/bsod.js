a6571d9ab7d0908fda5b3bdd9b4486014873999f
satanas <wil.alejandro@gmail.com>
2016-09-07 21:48:58 -0300

    Added bsod

---
+++ js/bsod.js
@@ -0,0 +1,34 @@
+var BSOD = function(t) {
+  var _ = this;
+  _.inherits(Sprite);
+  _.dt = t || 500;
+  Sprite.call(_, 0, 0, $.vw, $.vh);
+
+  // Start
+  _.s = function() {
+    _.ct = _.dt;
+    _.sw = 1;
+  };
+
+  _.u = function() {
+    _.ct -= $.e;
+    if (_.ct < 0) _.sw = 0;
+  };
+
+  _.r = function() {
+    if (!_.sw) return;
+
+    $.x.s();
+    $.x.fs("blue");
+    $.x.fr(_.x, _.y, _.w, _.h);
+    $.x.fs("#d3d3d3");
+    $.x.fr(250, 142, 140, 25)
+    $.x.ct('Exception', 20, 160, "blue", "courier");
+    $.x.ct('An exception 06 has occured at 0028:C13B3ADCF82 in', 18, 220, "#d3d3d3", "courier");
+    $.x.ct('VxD while locking 0028:C000459488. This was called', 18, 245, "#d3d3d3", "courier");
+    $.x.ct('by VMM(01) 00005913. Security system applications', 18, 270, "#d3d3d3", "courier");
+    $.x.ct('will be terminated immediately.', 18, 295, "#d3d3d3", "courier");
+    $.x.ct('*** STOP: 0x0000008E (0xC0000005 0x00690076)', 18, 420, "#d3d3d3", "courier");
+    $.x.r();
+  };
+};
--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -3,11 +3,22 @@ var MenuScene = function() {
   _.inherits(Scene);
   Scene.call(_);
   _.d = 100; // Delay before enabling the keyboard
+  _.sd = rndr(1000, 2000); // Screen Delay before showing the BSOD
   _.c = 0; // Counter
+  _.rdy = 0; // Ready to change scene
+
+  _.bs = new BSOD(100);
 
   _.update = function() {
     var _ = this;
-    if (_.c < _.d) _.c += $.e;
+    _.c += $.e;
+    if (_.c > _.d && !_.rdy) _.rdy = 1;
+    if (_.c > _.sd) {
+      _.bs.s();
+      _.sd = rndr(5000, 7000);
+      _.c = 0;
+    }
+
     $.x.clr('ivory');
 
     $.x.s();
@@ -16,7 +27,10 @@ var MenuScene = function() {
     $.x.ct("Created by @satanas82 for the js13k compo 2016", 14, 470, "gray");
     $.x.r();
 
-    if ($.i.p(13) && _.c >= _.d) {
+    _.bs.u();
+    _.bs.r();
+
+    if ($.i.p(13) && _.rdy) {
       _.exit();
       $.scn.game.start();
     }
