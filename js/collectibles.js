fced6b60ad6390efb9da0acaf769eead9bb49c4a
satanas <wil.alejandro@gmail.com>
2016-09-09 14:48:33 -0300

    Removed stuff from coin

--- js/collectibles.js
+++ js/collectibles.js
@@ -4,7 +4,7 @@ var Coin = function(x, y) {
   _.hs = [125, 265, 405];
   _.inherits(Sprite);
   Sprite.call(_, x, _.hs[y], 24, 24);
-  _.an = new Animator([0, 1, 2, 3], 100);
+  _.an = new Animator([0, 1, 2, 3], 80);
 
   _.u = function() {
     _.x -= $.e / 1000 * $.sp;
@@ -21,13 +21,11 @@ var Coin = function(x, y) {
       $.x.arc(_.x + (_.w / 2), _.y + (_.h / 2), 12, 0, PI * 2);
       $.x.f();
       $.x.fs("black");
-      $.x.sr(_.x + 9, _.y + 8, 6, 8);
     } else if (_.an.g() === 1 || _.an.g() === 3) {
       $.x.bp();
       $.x.e(_.x + 12, _.y + (_.h / 2), 7, 12, 0, 0, PI * 2);
       $.x.f();
       $.x.fs("black");
-      $.x.sr(_.x + 10, _.y + 8, 3, 8);
     } else if (_.an.g() === 2) {
       $.x.fr(_.x + 9, _.y, 6, _.h);
     }
