4f09caf0d6b78d560320ffae01bb67de8869d9e1
satanas <wil.alejandro@gmail.com>
2016-09-10 10:58:27 -0300

    Updating scenes instantiation

--- js/game.js
+++ js/game.js
@@ -13,11 +13,6 @@ window.addEventListener('load', function() {
   // Sounds
   //$.s.a('s', [0,,0.0989,0.3295,0.2402,0.4314,,,,,,0.4986,0.5758,,,,,,1,,,,,0.5]);
 
-  // Registering scenes
   $.scn.menu = new MenuScene();
-  $.scn.game = new GameScene();
-
-  $.scn.intro = new IntroScene();
-  $.scn.intro.loop();
-  //$.scn.menu.loop();
+  $.scn.menu.loop();
 });
