2523ede13de250512cf62977bd3fca89cd03bc39
satanas <wil.alejandro@gmail.com>
2016-09-10 15:57:14 -0300

    Fixed health render

--- js/hud.js
+++ js/hud.js
@@ -3,9 +3,14 @@ var HUD = function(p) {
   _.p = p; // Player instance
   _.h = 60;
   _.h1 = $.svg.n(data.h);
-  _.h2 = $.svg.n(data.h.replace('e22222', '999'));
+  _.h2 = $.svg.n(data.h.replace('e22222', 'ccc'));
   _.co = new Coin(168, 0);
   _.co.y = 18;
+
+  // Get heart image by index
+  _.gh = function(i) {
+    return (_.p.hp >= i) ? _.h1 : _.h2;
+  };
 
   _.u = function() {
   };
@@ -15,9 +20,9 @@ var HUD = function(p) {
     $.x.s();
     $.x.fs('rgba(30,30,30,0.9)');
     $.x.fr(0, 0, $.vw, _.h);
-    $.x.di(_.h1, 20, 18);
-    $.x.di(_.h1, 54, 18);
-    $.x.di(_.h1, 88, 18);
+    $.x.di(_.gh(1), 20, 18);
+    $.x.di(_.gh(2), 54, 18);
+    $.x.di(_.gh(3), 88, 18);
     $.x.ft('x', 20, 200, 35, 'white', 'sans-serif');
     $.x.ft($.cc.toString(), 28, 220, 37, 'white', 'courier');
     $.x.ft(floor($.dt / 10).toString() + ' mts', 28, 600, 37, 'white', 'courier', '', 'r');
