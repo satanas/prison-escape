b28a397c765cfcb13a100faeed1024c83e033ef1
satanas <wil.alejandro@gmail.com>
2016-09-10 10:59:00 -0300

    Added scripts for compilation

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  appName: 'prison-break',
+  sourceFiles: [
+    "js/engine/functions.js",
+    "js/engine/input.js",
+    "js/engine/camera.js",
+    "js/engine/scene.js",
+    "js/engine/group.js",
+    "js/engine/sprite.js",
+    "js/engine/collisions.js",
+    "js/engine/svg.js",
+    "js/engine/animator.js",
+    "js/engine/init.js",
+
+    "js/scenes/menu.js",
+    "js/scenes/intro.js",
+    "js/scenes/game.js",
+    "js/scenes/game_over.js",
+    "js/objects.js",
+    "js/collectibles.js",
+    "js/parallax.js",
+    "js/player.js",
+    "js/bullets.js",
+    "js/level.js",
+    "js/enemies.js",
+    "js/bsod.js",
+    "js/data.js",
+    "js/game.js"
+  ]
+}
